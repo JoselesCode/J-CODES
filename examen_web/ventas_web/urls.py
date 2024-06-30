845b31aae52ad769b7f430bf95c0ab62fa48cedb
JoseDiazGonzalez <joseantonio.diazg@hotmail.com>
2024-06-30 06:24:31 -0400

    creando crud 1

--- examen_web/ventas_web/urls.py
+++ examen_web/ventas_web/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cash-gta/', views.cash_gta, name='Cash_gta'),
+    path('crud-empleado/', views.crud_empleado, name='crud_empleado'),
+    path('formulario-contacto/', views.formulario_contacto, name='formulario_contacto'),
+    path('fornite/', views.fornite, name='fornite'),
+    path('index-login/', views.index_login, name='index_login'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('recuperar-contrasena/', views.recuperar_contrasena, name='recuperar_contrasena'),
+    path('rp-riot/', views.rp_riot, name='rp_riot'),
+    path('signup/', views.signup, name='signup'),
 ]
--- examen_web/ventas_web/views.py
+++ examen_web/ventas_web/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, 'ventas_web/index.html')
+
+def cash_gta(request):
+    return render(request, 'ventas_web/Cash_gta.html')
+
+def crud_empleado(request):
+    return render(request, 'ventas_web/crudempleado.html')
+
+def formulario_contacto(request):
+    return render(request, 'ventas_web/formulario-contacto.html')
+
+def fornite(request):
+    return render(request, 'ventas_web/Fornite.html')
+
+def index_login(request):
+    return render(request, 'ventas_web/indexLogin.html')
+
+def nosotros(request):
+    return render(request, 'ventas_web/nosotros.html')
+
+def recuperar_contrasena(request):
+    return render(request, 'ventas_web/recuperarC.html')
+
+def rp_riot(request):
+    return render(request, 'ventas_web/Rp_riot.html')
+
+def signup(request):
+    return render(request, 'ventas_web/signup.html')
