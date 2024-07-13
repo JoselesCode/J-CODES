0836803130da0235b266ecf7a81119f34136907a
JoseDiazGonzalez <joseantonio.diazg@hotmail.com>
2024-07-13 09:58:18 -0400

    ok 123

--- autenticacionUsuarios/account/forms.py
+++ autenticacionUsuarios/account/forms.py
@@ -8,15 +8,15 @@ class LoginForm(forms.Form):
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password',
                                widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password',
+    password2 = forms.CharField(label='Repeat Password',
                                 widget=forms.PasswordInput)
     
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
 
-    def clean_password(self):
+    def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            return forms.ValidationError('Las contraseñas no son iguales')
+            raise forms.ValidationError('Las contraseñas no son iguales')
         return cd['password2']
--- autenticacionUsuarios/account/urls.py
+++ autenticacionUsuarios/account/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     #path('login/', views.user_login, name='login')
     #path('login/', auth_views.LoginView.as_view(), name= 'login'),
     #path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
-    #path('password-change/', auth_views.PasswordChangeView.as_view(), name= 'Password_change'),
-    #path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name= 'Password_change_done'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name= 'Password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name= 'Password_change_done'),
     path('', include('django.contrib.auth.urls')), #con este llamo a todos los de arriba
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register')
